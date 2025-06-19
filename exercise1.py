83d1c49a4b5baf19c9d7fe70c97cb978472e613e
mstarkie <hikemike@gmail.com>
2025-06-19 14:34:30 -0400

    Initial Commit

---
+++ exercise1.py
@@ -0,0 +1,43 @@
+#@title Code - Load dependencies
+
+#general
+
+# machine learning
+import  keras
+# data
+import numpy as np
+import pandas as pd
+import seaborn as sns
+# data visualization
+from plotly.subplots import make_subplots
+from matplotlib import pyplot as plt
+
+
+# @title
+# The following code cell loads the dataset and creates a pandas DataFrame.
+# You can think of a DataFrame like a spreadsheet with rows and columns.
+# The rows represent individual data examples, and the columns represent the attributes associated with each example.
+chicago_taxi_dataset = pd.read_csv("https://download.mlcc.google.com/mledu-datasets/chicago_taxi_train.csv")
+
+# Updates dataframe to use specific columns.
+training_df = chicago_taxi_dataset[['TRIP_MILES', 'TRIP_SECONDS', 'FARE', 'COMPANY', 'PAYMENT_TYPE', 'TIP_RATE']]
+pd.set_option('display.width', 400)
+pd.set_option('display.max_columns', None)
+print('Read dataset completed successfully.')
+print('Total number of rows: {0}\n\n'.format(len(training_df.index)))
+print(training_df.head(200))
+training_df['TRIP_MILES'].plot(kind='hist', bins=20, title='TRIP_MILES')
+plt.gca().spines[['top', 'right',]].set_visible(False)
+plt.show()
+training_df['TRIP_SECONDS'].plot(kind='hist', bins=20, title='TRIP_SECONDS')
+plt.gca().spines[['top', 'right',]].set_visible(False)
+plt.show()
+training_df['FARE'].plot(kind='hist', bins=20, title='FARE')
+plt.gca().spines[['top', 'right',]].set_visible(False)
+plt.show()
+training_df['TIP_RATE'].plot(kind='hist', bins=20, title='TIP_RATE')
+plt.gca().spines[['top', 'right',]].set_visible(False)
+plt.show()
+training_df.groupby('PAYMENT_TYPE').size().plot(kind='barh', color=sns.palettes.mpl_palette('Dark2'), title='PAYMENT_TYPE')
+plt.gca().spines[['top', 'right',]].set_visible(False)
+plt.show()
